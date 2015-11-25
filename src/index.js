dd358d4e3b2c3295ee11f6cd26b9c337253e9491
stephen grider <ste.grider@gmail.com>
2015-11-25 15:15:49 -0800

    reorder imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/app';
