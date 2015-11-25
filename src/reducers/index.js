9128a89df4ecfbfbb4f58447eec92547f957fab3
stephen grider <ste.grider@gmail.com>
2015-11-25 15:26:51 -0800

    code cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({});
 
-export default rootReducer
+export default rootReducer;
