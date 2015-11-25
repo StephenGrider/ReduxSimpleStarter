dff784dd89f93b6df577b25eeec0005dbb9d59f8
stephen grider <ste.grider@gmail.com>
2015-11-25 14:05:00 -0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>React Simple Starter</div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: [
+    './src/main.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './'
+  }
+};
