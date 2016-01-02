14813f829dd6c5e70bce19e014841d550f873f69
stephen grider <ste.grider@gmail.com>
2016-01-01 16:19:59 -0800

    middleware setup included by default

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
