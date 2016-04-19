20539f83ac8f6a528de796026833dfe7973c32b4
Yale Yuen <yaleyyuen@gmail.com>
2016-04-19 11:06:20 -0700

    refactor index.js to use class based component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'; //used to create/manage components
+import React, { Component } from 'react'; //used to create/manage components
 import ReactDOM from 'react-dom'; //used to interact with actual dom
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyC8mevAQqji8_1A7-NkN9I6B7DxYpL2twY";
@@ -9,12 +9,26 @@ const API_KEY = "AIzaSyC8mevAQqji8_1A7-NkN9I6B7DxYpL2twY";
 
 //functional components used whenever taking in info and spitting out jsx, also funcitonal component can contain class based components
 //considered a class "App"
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'magic the gathering'}, (videos) => {
+      this.setState({ videos }); 
+      //aka
+      //this.setState({ videos: videos }); b/c of es6
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML. and put it on the page (in the DOM)
