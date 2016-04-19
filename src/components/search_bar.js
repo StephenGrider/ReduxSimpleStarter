ea4a1d536e2cfdf79260925d6c320f27f6fc6054
Yale Yuen <yaleyyuen@gmail.com>
2016-04-19 00:12:42 -0700

    refactor searchbar to class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,12 @@
-import React from 'react';
-
-const SearchBar = () => {
-  return <input />
-};
+import React, { Component } from 'react';
+                //gives searchbar all functionality from React.Component class
+class SearchBar extends Component {
+  //syntax for defining methods on a class
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
+
+//functional components vs class based components
