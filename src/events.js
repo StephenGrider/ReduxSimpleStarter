5a80b5f299e310012b6682eec37fd59b27efde75
Sam <thesamreaves@gmail.com>
2016-04-25 09:58:23 -0400

    Added pre-rendered events

---
+++ src/events.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    'title': 'All Day Event',
+    'allDay': true,
+    'start': new Date(2016, 4, 0),
+    'end': new Date(2016, 3, 0)
+  },
+  {
+    'title': 'Long Event',
+    'start': new Date(2016, 3, 7),
+    'end': new Date(2016, 3, 10)
+  },
+
+  {
+    'title': 'DTS STARTS',
+    'start': new Date(2016, 3, 13, 0, 0, 0),
+    'end': new Date(2016, 3, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'DTS ENDS',
+    'start': new Date(2016, 6, 6, 0, 0, 0),
+    'end': new Date(2016, 6, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'Some Event',
+    'start': new Date(2016, 4, 9, 0, 0, 0),
+    'end': new Date(2016, 4, 9, 0, 0, 0)
+  },
+  {
+    'title': 'Conference',
+    'start': new Date(2016, 4, 11),
+    'end': new Date(2016, 4, 13),
+    'desc': 'Big conference for important people'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2016, 4, 12, 10, 30, 0, 0),
+    'end': new Date(2016, 4, 12, 12, 30, 0, 0),
+    'desc': 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Lunch',
+    'start':new Date(2016, 4, 12, 12, 0, 0, 0),
+    'end': new Date(2016, 4, 12, 13, 0, 0, 0),
+    'desc': 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start':new Date(2016, 4, 12,14, 0, 0, 0),
+    'end': new Date(2016, 4, 12,15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start':new Date(2016, 4, 12, 17, 0, 0, 0),
+    'end': new Date(2016, 4, 12, 17, 30, 0, 0),
+    'desc': 'Most important meal of the day'
+  },
+  {
+    'title': 'Dinner',
+    'start':new Date(2016, 4, 12, 20, 0, 0, 0),
+    'end': new Date(2016, 4, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Birthday Party',
+    'start':new Date(2016, 4, 13, 7, 0, 0),
+    'end': new Date(2016, 4, 13, 10, 30, 0)
+  }
+];
