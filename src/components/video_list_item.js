aaccf50ed80822fc7b07e475c9f14d030a9c546e
Yale Yuen <yaleyyuen@gmail.com>
2016-04-27 12:06:57 -0700

    add videoSelection callback

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
   // const video = props.video; same as passing in props to line 3 
 
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className='list-group-item'>
+    <li onClick={() => onVideoSelect(video)} className='list-group-item'>
       <div className='video-list media'> 
         <div className='media-left'>
           <img className='media-object' src={imageUrl} />
--- src/index.js
+++ src/index.js
@@ -33,7 +33,9 @@ class App extends Component {
       <div>
         <SearchBar />
         <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
