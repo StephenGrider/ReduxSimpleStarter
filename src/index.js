364db169284af2e9f510c1b1437375859f58c2ec
Yale Yuen <yaleyyuen@gmail.com>
2016-04-27 14:53:20 -0700

    add lodsh and debounce to searchTerm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react'; //used to create/manage components
 import ReactDOM from 'react-dom'; //used to interact with actual dom
 import YTSearch from 'youtube-api-search';
@@ -33,9 +34,12 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})} //passCB to videoList
