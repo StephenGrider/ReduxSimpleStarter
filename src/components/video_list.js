f607d4cbd89ac70ee155e8f3c9606f332c284784
Noah Schutte <noahschutte@gmail.com>
2016-05-07 19:25:02 -0400

    fix onVideoSelect

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -5,7 +5,7 @@ const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
     return (
       <VideoListItem
-        onVideoSelect={props.onVideoClick}
+        onVideoSelect={props.onVideoSelect}
         key={video.etag}
         video={video} />
     );
