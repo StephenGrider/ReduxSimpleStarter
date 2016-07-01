6d5b395a543821de65baf9c17f0cad43aaa7fc29
Ben Fallon <bnfallon@gmail.com>
2016-07-01 08:50:04 -0500

    blank files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = "AIzaSyAaYVQqZdtfjDgAMsv_YWpFzb6zqZu1Y7w";
+
+//make a Component
+
+const App = () => {
+  return <div>Hello world!</div>;
+};
+
+//put compeont on screen
+ReactDOM.render(<App />, document.querySelector('.container'));
