a0f80c51756b12c274f5aec0a71914c53cfe86e4
Scott Vrable <scott@scottvrable.com>
2016-07-09 14:20:04 -0700

    Anal-retentive whitespace clean up, sp -> tabs, ' -> "

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import {renderComponent , expect} from "../test_helper";
+import App from "../../src/components/app";
 
-describe('App' , () => {
-  let component;
+describe("App" , () => {
+	let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	beforeEach(() => {
+		component = renderComponent(App);
+	});
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
+	it("renders something", () => {
+		expect(component).to.exist;
+	});
 });
