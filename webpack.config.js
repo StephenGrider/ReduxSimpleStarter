030f3117042a6d4bebfe496fe9ee0c91eceb2c8f
Scott Vrable <scott@scottvrable.com>
2016-08-08 22:56:36 -0700

    Change vars to consts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require("autoprefixer");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 const webpack = require("webpack");
 
 module.exports = {
