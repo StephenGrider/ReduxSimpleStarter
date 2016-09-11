3fee328512636ce1f0a7d1da2ce1f37720fcda8b
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-11 16:00:53 -0700

    (feat lecture23) Successfully pass props to VideoList

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCsr55vEo8jVDKmwFJ8yiDGnWA5PeuPJQU';
 
@@ -19,7 +20,13 @@ class App extends Component {
   }
 
   render() {
-    return <div><SearchBar/></div>;
+    // <VideoList videos={this.state.videos} />
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
   }
 }
 
