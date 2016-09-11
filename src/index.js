9e533821d54b2b5f3cc0db997d8fa7defb97d620
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-11 13:59:52 -0700

    (delete, lecture6) Delete boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create new component. Should produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
