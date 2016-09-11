c327ad5de130c90fabae8e47f476db6da89306bd
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-11 16:16:50 -0700

    (feat lecture26) Videos display on page

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map(video => <VideoListItem key={video.etag} video={video} />)
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
