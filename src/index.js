b6fd587f30a956834967cc2d8e0ad9ce9f81547b
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-11 16:43:22 -0700

    (feat lecture28) Single playable video loads to iframe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyCsr55vEo8jVDKmwFJ8yiDGnWA5PeuPJQU';
 
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
