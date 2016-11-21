f929de36e607f90f7fa8980e936470af9c92c9b8
sahilnayak <snayak.researcher@gmail.com>
2016-11-21 12:52:58 -0800

    Setting up webpack and React

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
+// Create a new component this component should create HTML
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
+const App = () => {
+    return <div>Hello</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
+// Take this generated HTML and put it on the page (Add to DOM)!!
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
