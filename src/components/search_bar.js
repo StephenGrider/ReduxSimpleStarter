685262ce97bae23dd2a8b492676c61015251445a
sahilnayak <snayak.researcher@gmail.com>
2016-11-22 14:01:30 -0800

    Adding in API

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {term: '' };
+
+    }
+
     render () {
-        return <input onChange={this.onInputChange}/>;
+         return (<div>
+           <input onChange={this.onInputChange.bind(this)}/>
+           Value of State = {this.state.term}
+        </div>);
     }
 
     onInputChange (event) {
-        console.log(event.target.value);
-
+        this.setState({term: event.target.value});
     }
 
 }
 
 export default SearchBar;
 
-console.log('changes');
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 // Create a new component this component should create HTML
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
-
-
+import VideoList from './components/video_list'
+import YTSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyDydhXviisO2az_6HksPtGyOjS9QPu30r0';
-const App = () => {
-    return (<div>
-        <SearchBar />
-    </div>);
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=>{this.setState({videos})});
+    }
+
+    render() {
+        return (<div>
+            <SearchBar />
+        </div>);
+    }
+
+
 }
 
 // Take this generated HTML and put it on the page (Add to DOM)!!
