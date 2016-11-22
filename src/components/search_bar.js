388b8403239629f7404e80488655ff561789c089
sahilnayak <snayak.researcher@gmail.com>
2016-11-22 08:43:54 -0800

    Adding in my components

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render () {
+        return <input onChange={this.onInputChange}/>;
+    }
+
+    onInputChange (event) {
+        console.log(event.target.value);
+
+    }
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // Create a new component this component should create HTML
 import React from 'react';
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+
+
+const API_KEY = 'AIzaSyDydhXviisO2az_6HksPtGyOjS9QPu30r0';
 const App = () => {
-    return <div>Hello</div>;
+    return (<div>
+        <SearchBar />
+    </div>);
 }
 
 // Take this generated HTML and put it on the page (Add to DOM)!!
