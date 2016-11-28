5a63fe39a28a4c3d013f3642906ba90aae84c162
Vaughn Walters <vaughnwalters@gmail.com>
2016-11-28 16:52:50 -0600

    hello world

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // Create a new component.  This component should produce some HTML
 const App = () => {
   return <div>Hello World!</div>;
-}
+} 
 
 // Take this component's generated HTML and put it in the DOM
 // first arg is instance of App function, and second arg is where to place the rendered HTML
