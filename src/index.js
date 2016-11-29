468711b322d08660a516b7c048c4b98b97588be5
Vaughn Walters <vaughnwalters@gmail.com>
2016-11-29 17:49:42 -0600

    youtube api search logging out with hardcorded data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyC_XvStCGm1z1suSkSnjeTkciTWMJQFjMc';
+
+YTSearch({key:API_KEY, term: 'surfboards'}, function(data) {console.log("data", data);
+});
 
 
 // Create a new component.  This component should produce some HTML
