91c7d8dff2b88b7dd63a54145f3ce82582b74d39
Josh Zylstra <zylstrajosh@gmail.com>
2016-12-27 18:50:08 -0800

    building youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: 'Starting Value'};
+  }
+  render() {
+    return <input
+    value={this.state.term}
+    onChange={this.onInputChange.bind(this)}/>;
+  }
+  onInputChange(event) {
+    console.log(this);
+    this.setState({term: event.target.value});
+  }
+}
 
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
@@ -7,8 +8,22 @@ const API_KEY = 'AIzaSyBs8HechEIQEcpnJLWHwNS_CYLEU5-XROA';
 
 // Create a new component
 // This component should produce some HTML
-const App = () => {
-  return <div>Hi!</div>;
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {videos: [] };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({videos: data});
+      console.log(this.state);
+    })
+  }
+  render(){
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
+  }
+}
 ReactDOM.render(<App />, document.getElementById('container'));
