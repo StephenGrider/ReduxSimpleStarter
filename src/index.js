2a0947b4ac47ed3161a5ca899b395867d0a71e60
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-04 16:35:53 +0200

    Api key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {config} from './config';
 
 const App = () => {
     return <div>Hi!</div>
 }
+console.log(config);
 
 ReactDOM.render(<App />, document.querySelector('.container'));
