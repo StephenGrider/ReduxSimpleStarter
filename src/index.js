5df36bb635cba579690adb6f3149b580b7eace39
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-04 15:58:29 +0200

    Render targets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = function() {
-    return <div>Hi!</div>;
+const App = () => {
+    return <div>Hi!</div>
 }
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
