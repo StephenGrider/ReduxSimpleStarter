289cbb91784cb0836a9271a0a643710ac7dee347
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-04 15:29:12 +0200

    Import statements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function() {
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+React.render(App);
