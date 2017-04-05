4a59baf7d57a0d460fb20caa5f00ee58ee804391
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 11:29:04 +0200

    State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value})} />
+            </div>
+        );
     }
 }
 
