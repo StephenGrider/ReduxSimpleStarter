5ff111b6adeee38d0b4a624fc2cdb1092879c5e2
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 11:15:58 +0200

    Event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={event => console.log(event.target.value)} />;
     }
 }
 
