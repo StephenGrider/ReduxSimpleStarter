5428315b0d85224bb028b3ab64b6cff6f35aa9bf
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 14:30:59 +0200

    Props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import {config} from './config';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 class App extends Component {
     constructor(props) {
@@ -13,10 +14,11 @@ class App extends Component {
             this.setState({ videos });
         });
     }
-    render() {        
+    render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
