88bbde427014928703f4660e33422ab947724d3f
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 10:59:44 +0200

    Class components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = function() {
-    return <input />;
-};
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
 
 export default SearchBar;
