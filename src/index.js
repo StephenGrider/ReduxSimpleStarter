a5bf29ddde79d08e43945e47352139d031f59756
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 11:49:57 +0200

    Querying YT api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import {config} from './config';
 import SearchBar from './components/search_bar';
+
+YTSearch({ key: config.API_KEY, term: 'surfboard' }, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
