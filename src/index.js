3fa0a55cc1144953cb2e6e7cb44088ddca77e264
Gabriele Antonini <gabriele.antonini@williamhill.com>
2017-04-05 11:59:18 +0200

    App to class component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import {config} from './config';
 import SearchBar from './components/search_bar';
 
-YTSearch({ key: config.API_KEY, term: 'surfboard' }, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = { videos: [] };
+        YTSearch({ key: config.API_KEY, term: 'surfboard' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {        
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
