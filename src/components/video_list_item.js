deb6ab29898bf568580798cdfbd019c238b77698
izi.versano@sociomantic.com <izi.versano@sociomantic.com>
2017-04-07 16:23:36 +0200

    Video list item implemented

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const VideoListItem = ( props ) => {
-    return <li>Video</li>;
+const VideoListItem = ( {video} ) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li className ="list-group-item">
+            <div className="video-list media"></div>
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+
+                 <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                 </div>
+            <div/>
+
+        </li>
+    );
 };
 
 
