5273112823a6af8124cdf5e39085a0a5dabaee58
Todd Rasband <Todd Rasband>
2017-04-09 14:50:31 -0600

    fixed

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+function selectBook(book) {
+  console.log('A book has been selected: ' + book.title);
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
+
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
-    );
+      <div>
+        <BookList></BookList>
+      </div>
+    )
   }
 }
