59b549a1dfea586cbbe6f57f699938a67a1e50d7
christopher-lamkin <cjlamkin64@gmail.com>
2017-05-20 13:26:07 -0700

    convert SearchBar from functional to class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
-  return <input/>;
-};
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
