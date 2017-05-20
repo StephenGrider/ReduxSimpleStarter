5db565639aaf591cf0191a8b88f87c1ead1a9dee
christopher-lamkin <cjlamkin64@gmail.com>
2017-05-20 13:45:42 -0700

    initialize state with a term property, set state to update term on input change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={event => console.log(event.target.value)}/>;
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''}
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />;
+      </div>
+    );
   }
 }
 
