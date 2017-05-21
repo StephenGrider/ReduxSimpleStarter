0f21114cfce255aa437701401c0da2157d24138a
christopher-lamkin <cjlamkin64@gmail.com>
2017-05-21 12:26:29 -0700

    creaet VideoList and VideoListItem components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAGMgh7QKPSGbjOV-JEVug4TCtyET_Vfuo';
@@ -19,6 +20,7 @@ class App extends Component {
     return (
       <div>
       <SearchBar />
+      <VideoList videos = {this.state.videos} />
       </div>
     );
   }
