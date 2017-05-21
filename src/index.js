3aa16f2a18c5b83de97c3d506bef3023fedb1798
christopher-lamkin <cjlamkin64@gmail.com>
2017-05-21 10:32:48 -0700

    convert App to class-based component, fetch data from youtube API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAGMgh7QKPSGbjOV-JEVug4TCtyET_Vfuo';
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props){
+    super(props);
 
-// Attach this component's generated html and put it on the page (DOM)
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render(){
+    return (
+      <div>
+      <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
