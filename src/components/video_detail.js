1f051c11c13388e2d684cdb34736b15706fca988
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2017-05-22 21:05:29 -0400

    handle slow ajax video load

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,6 +3,9 @@ import React from 'react';
 const VideoDetail = ({video}) => {
 	//const video = props.video;
 
+	if(!video){
+		return <div>Loading...</div>;
+	}
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;
 
