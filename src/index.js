33f286b8c3db8c7779e945a2633ef7627a205d89
Scott McCord <scott.sdm27@gmail.com>
2017-07-14 16:36:54 -0400

    set up react structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This component should produce
+// some HTML
+
+const App = () => {
+  return <div> Hi! </div>;
+}
+
+// Take this component's generated HTML and put it on
+// the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
