693e5bf16f5e672e0681475b562260ca7d99f89a
Scott McCord <scott.sdm27@gmail.com>
2017-07-15 11:49:39 -0400

    input element changes state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// define a new class called SearchBar and give it access to
+// all the functionality that a React.Component class has
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
 
-const SearchBar = () => {
-  return <input />;
-};
+    this.state = { term: '' };
+  }
+
+  // this is how the class renders JSX, every class must have
+  // a render function
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    )
+  }
+}
 
 export default SearchBar;
