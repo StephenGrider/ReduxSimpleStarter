08cb39c2e3f38a22b39f0031582c1fe9c6d6988a
Scott McCord <scott.sdm27@gmail.com>
2017-07-15 18:08:46 -0400

    got video to render to the page

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
-const VideoList = () => {
+const VideoList = (props) => {
+  const videoItems = props.videos.map(video => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 };
 
-export defaul VideoList;
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import videoList from './components/video_list';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCB9LjpB7-06mtXHbas8KDkQHoBA81733I';
 
 
@@ -27,6 +28,7 @@ class App extends Component {
   return (
     <div>
       <SearchBar />
+      <VideoDetail video={this.state.videos[0]}/>
       <VideoList videos={this.state.videos} />
     </div>
     )
