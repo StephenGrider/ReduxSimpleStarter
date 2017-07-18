c6321bb462c324fbc130170f1d500c5c0c15b686
Karl von Bonin <karlvonbonin@karls-mbp.hamburg.de.ibm.com>
2017-07-18 16:44:04 +0200

    fist steps

---
+++ src/containers/book_list.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+
+renderList() {
+	return this.props.books.map((book) => {
+		return(
+			<li key={book.title} className="list-group-item" >{book.title}</li>
+		);
+	});
+}
+
+	render() {
+		return (
+			<ul className="list-group col-sm-4">
+				{this.renderList()}
+			</ul>
+		)
+	}
+}
+
+
+function mapStateToProps(state) {
+	return{
+	  books: state.books
+
+	};
+
+}
+
+export default connect(mapStateToProps)(BookList);
