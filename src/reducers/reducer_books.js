2084ecc55a837645a77834bf38ee3bef5b7858b6
Alphavader <karlvonbonin@karls-mbp.hamburg.de.ibm.com>
2017-07-18 17:10:46 +0200

    fix

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,10 +3,6 @@ export default function () {
 		{title : 'JavaScript: The Good Parts'},
 		{title : 'Harry Potter'},
 		{title : 'Lord of the Rings'},
-<<<<<<< HEAD
-		{title : 'Hello Worlds'}
-=======
-		{title : 'Hello World'}
->>>>>>> aa4c6a4db91a962aad8393670dedea237e353735
+		{title : 'Hello World'},
 	];
 }
