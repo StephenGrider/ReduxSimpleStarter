c410747611ff8fba30c81d750ed642851798c960
Scott McCord <scott.sdm27@gmail.com>
2017-07-24 16:52:58 -0400

    finished tutorial

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 38 },
+    { title: 'Dark Tower', pages: 280 },
+    { title: 'Eloquent Ruby', pages: 1 }
   ]
 }
