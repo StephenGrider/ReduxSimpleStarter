98a9a9e74024a5c9fb6b655b499dbc0d585a56f5
Sean Brooks <sean@mhyway.com>
2017-08-26 15:21:07 -0500

    Fixed error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // Create a new component. This component should
 // produce some HTML
 
-const App = function {
+const App = function() {
   return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it on the page (in DOM)
+//
