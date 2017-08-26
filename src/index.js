3aa9fe6ce67b7c68be754a0c432309514af2e2d2
Sean Brooks <sean@mhyway.com>
2017-08-26 14:53:18 -0500

    Building components from scratch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component should
+// produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+const App = function {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in DOM)
