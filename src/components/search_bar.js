ad3c542f22c0e971676ec7318cdfc572dd3475af
Sean Brooks <sean@mhyway.com>
2017-08-26 22:58:59 -0500

    Made SearchBar aware of state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { term: ''};
+  }
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+        value={this.state.term}
+        onChange={(event) => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBRX8rhQ4Cklra0MV5Mgv4RP80-7bsKibw';
+
+YTSearch({ key: API_KEY, term: 'Elon Musk'}, function (data) {
+  console.log(data);
+})
 
 // Create a new component. This component should
 // produce some HTML
