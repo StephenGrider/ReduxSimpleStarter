ea488693f29d3f739d31e2b85851c2407e3e5990
Sean Brooks <sean@mhyway.com>
2017-08-27 00:34:17 -0500

    Able to display list of search response items

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBRX8rhQ4Cklra0MV5Mgv4RP80-7bsKibw';
 
-YTSearch({ key: API_KEY, term: 'Elon Musk'}, function (data) {
-  console.log(data);
-})
 
-// Create a new component. This component should
-// produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'Elon Musk'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos }/>
+      </div>
+    );
+  }
 }
-
-// Take this component's generated HTML and put it on the page (in DOM)
-//
 
 ReactDOM.render(<App />, document.querySelector('.container'));
