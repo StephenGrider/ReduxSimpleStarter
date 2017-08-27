75cc8a1fb23e9d90872c483676174873d38b66cc
Sean Brooks <sean@mhyway.com>
2017-08-27 02:30:44 -0500

    Added a lot'o'stuff.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,14 +5,20 @@ class SearchBar extends Component{
     super(props);
     this.state = { term: ''};
   }
+
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
         value={this.state.term}
-        onChange={(event) => this.setState({ term: event.target.value })} />
+        onChange={(event) => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBRX8rhQ4Cklra0MV5Mgv4RP80-7bsKibw';
 
@@ -11,18 +12,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({ key: API_KEY, term: 'Elon Musk'}, (videos) => {
-      this.setState({ videos });
+     this.videoSearch('Terraforming Mars');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <SearchBar onSearchTermChange={ term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo =>  this.setState({selectedVideo}) }
+          videos={ this.state.videos }/>
       </div>
     );
   }
