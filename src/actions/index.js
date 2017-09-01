1e57486f37618c25210efdc62b11208afe96fa56
Erik Verduin <everduin94@gmail.com>
2017-09-01 15:54:10 -0500

    In the middle of working with action creators

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+// Contains all action creators
+export function selectBook(book) {
+  console.log('a book has been selected', book.title);
+}
+
+// Have to wire to redux
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+  books: BooksReducer
+}); // This is the mapping of our state
 
 export default rootReducer;
