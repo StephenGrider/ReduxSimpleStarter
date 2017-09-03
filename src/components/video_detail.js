8c9561b9c91054e651e4343061c2722c234fca97
pennwu1 <phw.dental@gmail.com>
2017-09-03 11:05:21 -0700

    create current video functionality

---
+++ src/components/video_detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoDetail = ({video, onVideoSelect}) => {
+  if (!video) return <div>Loading...</div>;
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-reponsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
