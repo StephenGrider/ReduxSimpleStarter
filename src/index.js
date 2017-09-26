b596656ec7ee40581f9178fbaf6d4df6861d0e8b
Ryan Trainor <rptrainor@gmail.com>
2017-09-26 14:44:02 -0700

    updated readme.md

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// create a new component. this component should produce 
+// some html
+const App = function() {
+	return <div>Hi</div>;
+}
+
+
+
+
+// that this component's generated html and put on 
+// on the page (like the DOM)
