fe4bf01d4c39c3cb702c3ff97a7dbd3106352edc
Ryan Trainor <rptrainor@gmail.com>
2017-09-27 19:54:23 -0700

    in process of debugging youtube api connection issue with key

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const 	API_KEY = 'AIzaSyDavNZWlHpZkkzLNpSWSn9rqwzIlQjgdyY';
+import SearchBar from './components/searchBar';
+
+const API_KEY = 'AIzaSyBEpM6_kQHzfXlBgvyVeUBpB84obomxsbk';
 
 
 // create a new component. this component should produce 
 // some html
-const App = function() {
-	return <div>Hi</div>;
+const App = () => {
+    return (
+	    <div>
+	    	<SearchBar />
+	    </div>
+	    );
 }
 
 
 
-
-// that this component's generated html and put on 
-// on the page (like the DOM)
+//that this components generated html and put on the page (like the DOM)
