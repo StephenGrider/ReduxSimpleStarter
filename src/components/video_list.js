3c325a096f6d677c570c409c7d4cced1491a5ad3
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-11-30 19:35:32 -0800

    Create video list component that currently displays the number of videos it received from API call.

---
+++ src/components/video_list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const VideoList = (props) => {
+  return (
+    <ul className = 'col-md-4 list-group'>
+      {props.videos.length}
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { API_KEY } from './Dev_secret';
-import YTSearch from 'youtube-api-search'
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     
     this.state = { 
       videos: []
     };
     
-    YTSearch({key:API_KEY, term: 'surfboards'}, videos => this.setState({ videos }))
+    YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => { 
+      this.setState({ videos })
+    });
   }
 
   render () {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+ReactDOM.render(<App />, document.querySelector('.container'));
