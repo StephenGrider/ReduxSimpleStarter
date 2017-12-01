78d937c2b07472e2c4a1676637f8e947971dff34
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-11-30 21:22:38 -0800

    Create list of pulled youtube videos.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item'
 
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem  key = {video.etag} video = {video} />
+  });
+
   return (
     <ul className = 'col-md-4 list-group'>
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
