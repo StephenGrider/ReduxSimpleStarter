b647ad91c777a173b27d937bf3261be5a42a1325
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-11-30 16:29:21 -0800

    Create serachBar Component file with input field.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { API_KEY } from '../src/Dev_secret.js'
-
+import { API_KEY } from './Dev_secret';
+import SearchBar from './components/search_bar'
 //Create a new component.  This component should produce some HTML.
 
+
 const App = () => {
-  return(
-    <div>Hi! { API_KEY } </div>
-  )
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take the component's generated HTML and put it on the page (in the DOM)
