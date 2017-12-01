a94aa5bbda8d65a89ad3d2eef91968662b462bbe
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-11-30 17:10:48 -0800

    Add state to searchBar and update onChange event handler to update search bar state.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+  }
   render() {
     return (
-    <input onChange={e => console.log(e.target.value)}/>
+    <input onChange={e => this.setState({ term: e.target.value })} />
   )}
 
 }
