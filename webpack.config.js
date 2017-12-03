eb21ac782e6ed1583898758e3e87af66a2e79e43
guilima <gui.lima30@gmail.com>
2017-12-03 12:01:37 -0200

    Change babel preset es2015 to env

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015', 'stage-1']
+        presets: ['react', 'env', 'stage-1']
       }
     }]
   },
