a736271b3a3fa0aead40b125da290d2b30e44f07
Andres Saldarriaga <jandreus87@hotmail.com>
2018-01-25 12:59:44 -0500

    complete

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,10 @@
 import _ from "lodash";
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
-    
+    case DELETE_POST:
+      return _.omit(state, action.payload);
 
     case FETCH_POST:
       // const post = action.payload.data;
