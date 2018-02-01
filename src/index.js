68bbbced3b1a66327078d49258e5819c4ad0fc27
ccm36 <cameron.c.malloy@gmail.com>
2018-02-01 11:59:42 -0500

    Render index.js to page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { YOUTUBE_API_KEY } from './config.js';
+
+const App = () => {
+  return (
+    <div>Hi!</div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
