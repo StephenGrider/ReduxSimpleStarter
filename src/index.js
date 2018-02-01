8d7ed0fd81885d944c764e2c053beb2289b1d5e6
ccm36 <cameron.c.malloy@gmail.com>
2018-02-01 12:01:10 -0500

    Create Search component and add to page

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { YOUTUBE_API_KEY } from './config.js';
 
+import SearchBar from './components/SearchBar.js'
+
 const App = () => {
   return (
-    <div>Hi!</div>
+    <div>
+      <SearchBar />
+    </div>
   );
 }
 
