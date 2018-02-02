e63ed56a269787b4d55016da4cd3e26eacf60d2e
ccm36 <cameron.c.malloy@gmail.com>
2018-02-02 12:31:45 -0500

    Add preliminary styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={e => this.setState({ term: e.target.value })} />
