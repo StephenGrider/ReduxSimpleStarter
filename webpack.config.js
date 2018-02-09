e617ca44fc0c77bc69dccc8d476518a5f0b9b04a
Phan Thanh <thanhpd56@gmail.com>
2018-02-09 14:06:51 +0700

    fix: loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,14 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
