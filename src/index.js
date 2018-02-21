b4a04e3a0899d50f0b60d68704f6e82c882fdb4b
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-02-21 15:23:50 +0100

    update webpack and add hmr

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './components/app';
 import reducers from './reducers';
 
+import style from '../style/style.css'
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const render = Component => {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <AppContainer>
+        <App />
+      </AppContainer>
+    </Provider>
+    , document.querySelector('.container')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./components/app', () => {
+    render(App)
+  })
+}
