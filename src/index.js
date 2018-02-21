f5bd0e1375134ae228b8a9826617a246d58c5daa
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-02-21 16:02:30 +0100

    update store config for HMR

--- src/index.js
+++ src/index.js
@@ -10,22 +10,35 @@ import reducers from './reducers';
 import style from '../style/style.css'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 const render = Component => {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
       <AppContainer>
-        <App />
+        <Component />
       </AppContainer>
     </Provider>
     , document.querySelector('.container')
-  )
+  );
 }
 
-render(App)
+/* Intital rendering of the component */
+render(App);
 
+
+/* Could use an extra ENV check for development */
 if (module.hot) {
+  /*
+    Full page reload on reducer change
+    changelog: https://github.com/reactjs/react-redux/releases/tag/v2.0.0
+    explanation: https://github.com/gaearon/react-hot-loader/issues/413
+  */
+  module.hot.accept('./reducers', () => {
+    store.replaceReducer(reducers)
+  });
+  /* HMR reload on component change. */
   module.hot.accept('./components/app', () => {
-    render(App)
-  })
+    render(App);
+  });
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state: (state = {}) => state,
+  weather: (state = {}) => state,
 });
 
 export default rootReducer;
