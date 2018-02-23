1b2f7f9ac39ce909f5e3ec0fa983b99b64186ec4
Emmanuel Vilsbol <emmanuel@vilsbol.com>
2018-02-23 15:28:58 +0100

    clean reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   state: (state = {}) => state,
-  weather: (state = {}) => state,
 });
 
 export default rootReducer;
