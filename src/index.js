c1d8f310ef01f411d40189a3e5881bb8660425f2
Travis Hoover <traviskhoover@gmail.com>
2018-03-04 18:59:36 -0600

    Create a separate file for all of the components

--- src/index.js
+++
@@ -1,4 +0,0 @@
-// Create a new component. This component will produce HTML
-
-
-// Take the component's HTML and put it in the DOM
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+/* eslint-env browser */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Hi!</div>;
+
+ReactDOM.render(<App />, document.querySelector('.container'));
