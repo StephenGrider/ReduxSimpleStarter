841a82394dc495576982c2b335f188db33da8f80
Travis Hoover <traviskhoover@gmail.com>
2018-03-05 22:12:04 -0600

    Get videos from Youtube and use list and list-item components to display a list of videos and each individual video, respectively.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,30 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDMKHcl27WCHl40nZYAIuxP65vRQVJIUp8';
 
-const App = () =>
-  (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
