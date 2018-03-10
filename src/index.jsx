1a1dced57249354b187a445e3d1489c64c3d9ace
Travis Hoover <traviskhoover@gmail.com>
2018-03-09 22:28:51 -0600

    Show video in an iframe player. Handle null props.

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDMKHcl27WCHl40nZYAIuxP65vRQVJIUp8';
 
@@ -13,7 +14,7 @@ class App extends React.Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+    YTSearch({ key: API_KEY, term: 'heroes of the storm' }, (videos) => {
       this.setState({ videos });
     });
   }
@@ -21,6 +22,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
