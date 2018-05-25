e955807be33511398012d477d8d749083e9484a7
Frédéric Blanc <frederic.blanc@coservit.fr>
2018-05-25 17:52:07 +0200

    Commit après 23 scéance

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import * as ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyB5BKTc69oBzFC65U2OsNuC0QCMsgTSx2I';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = {
+            videos: [],
+            selectedVideo: null,
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0],
+            });
+        });
+    }
+
+    selectVideo(selectedVideo) {
+        this.setState({selectedVideo});
+    }
+
+    render() {
+        return (
+            <div>
+                Hello world
+                <SearchBar apiKey={API_KEY}/>
+                <VideoList
+                    videos={this.state.videos}
+                    onVideoSelect={this.selectVideo.bind(this)} />
+                <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
