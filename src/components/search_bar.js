a2ee380a81b3be935c1698ff786f45f00046ab56
Pablo Hernandez <prh90@yahoo.com>
2018-07-02 21:18:59 -0700

    Adds value to bar after state is set

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,9 @@ class SearchBar extends Component {
   render() {
     return(
       <div>
-        <input onChange={ (event) => this.setState({ term: event.target.value }) } />
+        <input
+        value={this.state.term}
+        onChange={ (event) => this.setState({ term: event.target.value }) } />
       </div>
     );
   }
