285d8f9866f1be5b68e79e83dc5e493091289eff
Pablo Hernandez <prh90@yahoo.com>
2018-07-02 20:49:11 -0700

    Adds set state to update the state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }
+  }
+
   render() {
-    return <input />;
+    return(
+      <div>
+        <input onChange={ (event) => this.setState({ term: event.target.value }) } />
+      </div>
+    );
   }
 }
 
