93e895b4aeebf7382142be1b1b47474f47232d23
Pablo Hernandez <prh90@yahoo.com>
2018-07-06 20:40:51 -0700

    Adds constructor for videos callback info in array using state

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = config.SECRET_KEY;
 
+
 // Create new component and it should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // Condensed version of this.setState({ videos: videos})
+    });
+  }
+
+
+  render()  {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take component and put it onto page(DOM)
