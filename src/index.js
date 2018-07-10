7f8f78ab482df50bba186222c9d6c647a1cf4185
Pablo Hernandez <prh90@yahoo.com>
2018-07-09 17:44:28 -0700

    adds iterator and function for each individual item in the list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = config.SECRET_KEY;
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
