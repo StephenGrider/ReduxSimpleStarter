798ac98fa0d03aac13aaecd33758cc0f9b61e64f
Pablo Hernandez <prh90@yahoo.com>
2018-07-10 17:54:30 -0700

    Adds ability to show image and title of videos

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  // const video = porps.video; identtical to what is being passed in instead of
+  // props and use props.video. It says there is a property of video coming in
+  // so declare it with a variable of the same name
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+
+        <div className="media-left">
+          <img className="mdeia-object" src={imageUrl}/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
