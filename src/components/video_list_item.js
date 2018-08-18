949423f64b66fa44e777b91550d34acb835d15d7
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 14:57:09 -0500

    clicking on the videos in the side bar to the right changes the video rendered to the page that is playable

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
     // const video = props.video; ^ same as ({video})  only (props) needed as paramater. 
+    // const onVideoSelect  = props.onVideoSelect 
     const imageUrl = video.snippet.thumbnails.default.url;
-
+    
     return (
-    <li className = 'list-group-item'>
+    <li onClick = {() => onVideoSelect(video)} className = 'list-group-item'>
         <div className = "video-list media">
           <div className = 'media-left'>
             <img className = 'media-object' src={imageUrl} alt=""/>
--- src/index.js
+++ src/index.js
@@ -27,18 +27,24 @@ class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null 
         }
         searchYouTube({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos })
+            this.setState({ 
+                videos, 
+                selectedVideo: videos[0]
+             })
         });
     }
     render() {
         return (
             <div>
               <SearchBar />
-              <VideoDetail video={this.state.videos[0]} />
-              <VideoList videos ={this.state.videos}/>
+              <VideoDetail video={this.state.selectedVideo} />
+              <VideoList 
+                onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                videos ={this.state.videos}/>
             </div>
         )
     }
