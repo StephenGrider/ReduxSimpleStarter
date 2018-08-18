184eeb39cdb8dec8fabbc210f42ef174a87e535f
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 15:39:53 -0500

    made the video size that is playable bigger

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -8,7 +8,7 @@ const VideoDetail = ({video}) => {
   return (
       <div className="video-detail col-md-8">
         <div className ="emed-responsive embed-responsive-16by9" >
-          <iframe className = "embed-responsive-item"src={url}></iframe>
+          <iframe className = "embed-responsive-item video-frame"src={url}></iframe>
         </div>
         <div className ='details'>
           <div>{video.snippet.title}</div>
