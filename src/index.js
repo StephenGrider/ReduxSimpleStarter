e9d97036f0338775a1f850d529e6f65386f7021c
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 13:26:26 -0500

    there is two different ways to get the data but the data logs to the console as it should from youtube.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
+import YTSearch from 'youtube-api-search';
+import searchYouTube from 'youtube-api-search'; 
+
 
 import SearchBar from './components/search_bar'
 
 
-const API_KEY = 'AIzaSyBXPqnYW52pjXyvrXmLyvNJ1P20Qarncnk ';
+const API_KEY = 'AIzaSyBXPqnYW52pjXyvrXmLyvNJ1P20Qarncnk';
 
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data)
+});
+//Or like this
+
+
+
+searchYouTube({key: API_KEY, term: 'surfboard', maxResults: 3}, (videos) => {
+    console.log(videos)
+});
+
+//Like this or^^^^
 
 //Create a new component. This component should produce some HTML
 // 
