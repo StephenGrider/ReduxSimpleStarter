956d72c58ccb4d10e917209fd5d7d3260a1310f4
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 14:26:28 -0500

    Rendering video snippets to the page a total of five

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-    const videoItems = props.videos.map((video, i)=> <VideoListItem key = {i} video={video}/>)
+    const videoItems = props.videos.map(video=> <VideoListItem key = {video.etag} video={video}/>)
     return (
         <ul className="col-md-4 list-group">
           {videoItems}
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({video}) => {
+    // const video = props.video; ^ same as ({video})  only (props) needed as paramater. 
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+    <li className = 'list-group-item'>
+        <div className = "video-list media">
+          <div className = 'media-left'>
+            <img className = 'media-object' src={imageUrl} alt=""/>
+          </div>
+          <div className ='media-body'>
+              <div className = 'media-heading'>
+                {video.snippet.title}
+              </div>
+          </div>
+        </div>
+    </li>
+    )
 };
 
 export default VideoListItem;
