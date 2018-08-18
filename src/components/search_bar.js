65d12275793c489f62290c11e6f1d56358065412
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 15:12:57 -0500

    added in some styling on hover background of sidebar thumbnails change and cursor and search bar comes across the page.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,7 @@ class SearchBar extends Component {
 
     render () {
         return (
-        <div>
+        <div className = "search-bar">
           <input value ={this.state.term} onChange={this.onInputChange} />
          
         </div>
