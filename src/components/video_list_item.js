81de33d19c25d884b96cf68ae7419dfbb265bd3e
codejoncode <jonathanjholloway@gmail.com>
2018-08-18 14:13:48 -0500

    starting to render the video list and list item page. List item is just a li while video list gets the data and produces a list item for each video.

---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const VideoListItem = (props) => {
+    return <li>Video</li>
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'; 
 import YTSearch from 'youtube-api-search';
 import searchYouTube from 'youtube-api-search'; 
 
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 
 
 const API_KEY = 'AIzaSyBXPqnYW52pjXyvrXmLyvNJ1P20Qarncnk';
 
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data)
-});
+// YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+//     console.log(data)
+// });
 //Or like this
 
 
 
-searchYouTube({key: API_KEY, term: 'surfboard', maxResults: 3}, (videos) => {
-    console.log(videos)
-});
+
 
 //Like this or^^^^
 
-//Create a new component. This component should produce some HTML
-// 
-const App = () => {
-    return (
-        <div>
-          <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            videos: []
+        }
+        searchYouTube({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })
+        });
+    }
+    render() {
+        return (
+            <div>
+              <SearchBar />
+              <VideoList videos ={this.state.videos}/>
+            </div>
+        )
+    }
 }
 
-//Take this components generated HTML and put it on the page (in the DOM)
+
 
 
 ReactDOM.render(<App />, document.querySelector('.container'))
