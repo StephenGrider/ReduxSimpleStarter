402f6855b64e90e26ec7b13159d0e7c42e293361
MonikaP <piechatzek@hotmail.com>
2018-09-12 22:58:08 -0600

    added index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDJowKxUE5Ooe6U36I0MHvXBgVHevncMV4';
+
+const App = () => {
+	return <div>hiiii</div>;
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
