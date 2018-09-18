15df7d4febf8998de6f2e68aef4156d65fb86f7b
versusniubility <versusniubility@gmail.com>
2018-09-17 20:02:33 -0700

    render targets

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Create a new component. This component should produce
 // some HTML
-const App = function(){
+const App = () => {
   return <div>Hi!</div>;
 }
 
