cba3748300be6e518f73a1f9b665251ff6ff1c9b
versusniubility <versusniubility@gmail.com>
2018-09-17 23:03:27 -0700

    state import

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {term: ' '};
+  }
   render(){
     return <input onChange ={event => console.log(event.target.value)}/>;
   }
