5f9f8776181754a9b27d9b9e9bc4a7823a24a927
versusniubility <versusniubility@gmail.com>
2018-09-17 18:30:48 -0700

    add one line

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Create a new component. This component should produce
+// some HTML
+const App = function(){
+  return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and
+// put is on the page(in DOM)
+React.render(App);
