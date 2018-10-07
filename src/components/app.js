04c32caf465a5c6e0129546f74a99ab168459e4b
NIKHIL <nikhilvaidyar1997@gmail.com>
2018-10-07 23:51:38 +0530

    Text Change
    I like to change react over redux because that suits more on it.

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>Redux simple starter</div>
     );
   }
 }
