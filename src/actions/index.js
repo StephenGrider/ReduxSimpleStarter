6b7550bff18f0b10317e6e57015748fddc010902
oscarOficial <oscar.casais.lopez@gmail.com>
2018-10-19 20:23:53 +0200

    Remove/add new dependencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,9 @@
-import axios from 'axios';
-
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = "FETCH_POST";
 export const DELETE_POST = "DELETE_POST";
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=oskitar1234';
-
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
     return {
         type: FETCH_POSTS,
         payload: request
@@ -18,9 +11,6 @@ export function fetchPosts() {
 }
 
 export function createPost(values, callBack) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
-        .then(callBack)
-
     return {
         type: CREATE_POST,
         payload: request
@@ -28,19 +18,13 @@ export function createPost(values, callBack) {
 }
 
 export function fetchPost(id) {
-    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-
     return {
         type: FETCH_POST,
         payload: request
     }
 }
 
-export function deletePost(id, { succesCallBack: success, errorCallBack: error }) {
-    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
-        .then(success)
-        .catch(error);
-
+export function deletePost(id) {
     return {
         type: DELETE_POST,
         payload: id
