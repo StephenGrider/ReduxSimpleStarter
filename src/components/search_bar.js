5d3ea2359282e087bbd6d640c3b51afbca2f27e8
Katia <katiapunter@gmail.com>
2018-11-02 11:49:42 +0000

    key logging functionality temporary

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input />
+    return <input onChange={this.onInputChange} />
   }
-
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
 };
 
 export default SearchBar;
