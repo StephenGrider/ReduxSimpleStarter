2893821e8a1ac2337ad95c80725545acd3830b67
Katia <katiapunter@gmail.com>
2018-11-02 11:11:17 +0000

    imports and adds search bar to  index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'search-bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCgpmgwIjb9S6vWjLy-jV2Ep9eYpFR7byk'
 
 const App = () => {
-  return <div>Hello World from kkatia</div>
-};
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
