596cc67d925fc5076e896fc810a7fde12054da71
Katia <katiapunter@gmail.com>
2018-11-02 11:21:37 +0000

    switches from basic component to class component for search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />
+class SearchBar extends Component {
+  render() {
+    return <input />
+  }
+
 };
 
 export default SearchBar;
