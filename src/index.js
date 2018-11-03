529d089147973ec44e2571369286c4b2c99f0b56
Althea <altheaserena.yi@gmail.com>
2018-11-03 16:36:40 -0400

    Sets up redux to pass dispatches to actions

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//  Allows redux to pass dispatches to actions
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={ createStoreWithMiddleware(reducers) }>
     <App />
   </Provider>
   , document.querySelector('.container'));
