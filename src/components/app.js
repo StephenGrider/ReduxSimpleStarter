6c7d0e6b6cdd2f20c7d86144fcf7b3959fbed69e
Althea <altheaserena.yi@gmail.com>
2018-11-03 18:11:51 -0400

    Sets up component to render list of patients

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+let actions = require('../actions/index');
 
-export default class App extends Component {
+export class App extends Component {
+  componentWillMount() {
+    this.props.fetchPatient();
+  }
+
   render() {
-    return (
-      <div>React simple starter</div>
-    );
+    let { patients } = this.props
+
+    if (patients.isFetching == true) {
+      return <p>Loading</p>
+    } else if (patients.isFetching == false && patients.patientsArray.length >=1) {
+      return (
+        <div>
+          <div className="dev-list">
+            { patients.patientsArray.map((patient) =>
+              <li key={ patient.id }>
+                { patient.attributes.firstName } { patient.attributes.lastName }
+              </li>)
+            }
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <p>PLEASE WORK</p>
+      )
+    }
   }
 }
+
+export default connect((state)=>{ return state }, actions)(App);
