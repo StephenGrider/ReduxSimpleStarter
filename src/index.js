3614a07b8c84b827647b2eb6580dfccca45dbe1c
Katia <katiapunter@gmail.com>
2018-11-04 09:24:26 +0000

    adds first search functionality for hardcoded term snowboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCgpmgwIjb9S6vWjLy-jV2Ep9eYpFR7byk'
+
+YTSearch({ key: API_KEY, term: 'snowboard' }, (data) => { console.log(data) });
 
 const App = () => {
   return (
