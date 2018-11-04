1fdee29f037a77302a3cec23c0dff6ddf97e2e6b
Katia <katiapunter@gmail.com>
2018-11-04 11:55:07 +0000

    logs api call result with 5 videos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCgpmgwIjb9S6vWjLy-jV2Ep9eYpFR7byk'
 
-YTSearch({ key: API_KEY, term: 'snowboard' }, (data) => { console.log(data) });
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'snowboard' }, (videos) => this.setState({ videos }));
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <SearchBar />
+      </div >
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
