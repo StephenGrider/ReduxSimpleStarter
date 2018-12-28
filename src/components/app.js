21b1e30ecaafff8eef92626a7d6965858916bb5d
Paul Gi <paulsgi@PAUL-MAC.local>
2018-12-27 23:18:29 -0800

    eslint changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>React simple starter</div>
     );
   }
 }
+
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state: (state = {}) => state,
 });
 
 export default rootReducer;
