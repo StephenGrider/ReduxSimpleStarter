f2bad4d5c5c79606b98f0dc480a8c49a7a4695eb
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-31 00:43:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search';
-const API_KEY = 'AIzaSyBZO62YgvAL3d0JIJnT5VQssG_jWV-94sg';
+
 
 // Create a new Component. This component should produce 
 // some HTML
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-   console.log(data);
-});
 
 class App extends Component {
+
+    constructor(props){
+     super(props);
+
+     this.state = { videos:[]}
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+          this.setState({ videos});
+          //this.setState({ videos: videos});
+        });
+    }
+
+
+
 	render() {
 	 return (
 	    <div>
