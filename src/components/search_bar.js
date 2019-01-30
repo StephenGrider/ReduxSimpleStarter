6521817d336ca6c6575e23d3a7a846130144c280
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-30 20:26:03 +0530

    Update and rename app.js to search_bar.js

---
+++ src/components/search_bar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+   
+   constructor(props) {
+   	super(props);
+
+   	this.state = { term : ''};
+   }
+
+
+	render(){
+		return(
+			<div>
+		      <input onChange={event => this.setState({ term: event.target.value}) }/>
+		    </div>
+		);
+	}
+}
+
+export default SearchBar;
