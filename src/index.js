c0ba9c2fcb6d9f469f954e53a7319e997c1c10e7
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-31 00:32:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
-
-YTSearch = ({key: API_KEY, term: 'surfboards'}, function(data) {
-   console.log(data);
-});
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyBZO62YgvAL3d0JIJnT5VQssG_jWV-94sg';
 
 // Create a new Component. This component should produce 
 // some HTML
-const App = () => {
-	return (
-	  <div>
-	   <SearchBar />
-	  </div>
-    );	
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+   console.log(data);
+});
+
+class App extends Component {
+	render() {
+	 return (
+	    <div>
+	      <SearchBar />
+	    </div>
+     );	
+    }
 }
 
 
