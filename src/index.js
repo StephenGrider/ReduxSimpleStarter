5eda916d00be2f180fccbbca9f5fdd830af02528
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-31 00:16:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 
-
+YTSearch = ({key: API_KEY, term: 'surfboards'}, function(data) {
+   console.log(data);
+});
 
 // Create a new Component. This component should produce 
 // some HTML
