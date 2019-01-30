1e7bbc61f5a1e56eff1815f9e785a86dc168f495
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-30 20:41:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyBZO62YgvAL3d0JIJnT5VQssG_jWV-94sg';
+
 
 // Create a new Component. This component should produce 
 // some HTML
