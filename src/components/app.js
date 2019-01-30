3f877efee861fabe69b309294b56810c23199c0c
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-30 20:24:24 +0530

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+const API_KEY = 'AIzaSyBZO62YgvAL3d0JIJnT5VQssG_jWV-94sg';
+
+// Create a new Component. This component should produce 
+// some HTML
+const App = () => {
+	return (
+	  <div>
+	   <SearchBar />
+	  </div>
+    );	
 }
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
