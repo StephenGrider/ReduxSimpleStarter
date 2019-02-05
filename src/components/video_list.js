3baba195a342ba9468c79d8d471b06ff304f80c4
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 19:23:54 +0530

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,9 +2,9 @@ import React from 'react';
 import VideoListItem from './video_list_item'; 
 
 const VideoList = (props) => {
-   const videoItems = props.videos.map((video)) => {
-   	return <VideoListItem video={video} />
-   }
+   const videoItems = props.videos.map((video) => {
+   	return <VideoListItem key={video.etag} video={video} />
+   });
 
    return(
      <ul className="col-md-4 list-group">
