4763c066011058f48d6d2ed60b83cbecb195e672
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 16:51:10 +0530

    Update video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import VideoListItem from './video_list_item'; 
 
 const VideoList = (props) => {
    return(
      <ul className="col-md-4 list-group">
-       {props.videos.length}
+       
      </ul>
    	);
 };
-export default VideoList;
+
+export default VideoList; 
