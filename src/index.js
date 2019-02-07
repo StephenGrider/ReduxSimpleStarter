2d618e3799923e64369a2b8926c5de580691f846
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-07 23:06:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBZO62YgvAL3d0JIJnT5VQssG_jWV-94sg';
 
 // Create a new Component. This component should produce 
 // some HTML
@@ -13,7 +14,7 @@ class App extends Component {
     constructor(props){
      super(props);
 
-     this.state = { videos:[]}
+     this.state = { videos:[]};
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
           this.setState({ videos});
@@ -27,7 +28,8 @@ class App extends Component {
 	 return (
 	    <div>
 	      <SearchBar />
-		 <VideoList videos={this.state.videos}/>
+	      <VideoDetail video={this.state.video[0] />
+	      <VideoList videos={this.state.videos} />
 	    </div>
      );	
     }
