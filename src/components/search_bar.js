382fb767e5db6c464ff9a0ff0f62e52c7afacf92
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-08 15:55:55 +0530

    Update search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 
 	render(){
 		return(
-			<div>
+			<div className="search-bar">
 			 <input
 			  value = {this.state.term}
 		      onChange={event => this.setState({ term: event.target.value}) }/>
