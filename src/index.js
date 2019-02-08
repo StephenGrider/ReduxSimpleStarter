c728770bd45edb453b954bc86b3b36530cb1d05d
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-08 14:17:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,7 +35,9 @@ class App extends Component {
 	    <div>
 	      <SearchBar />
 	      <VideoDetail video={this.state.selectedVideo}  />
-	      <VideoList videos={this.state.videos} />
+	      <VideoList 
+          onVideoSelect={selectedVideo = this.setState({selectedVideo})}
+	      videos={this.state.videos} />
 	    </div>
      );	
     }
