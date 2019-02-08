9781a18d4719f6c898e7194690d620584b459886
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-08 14:24:27 +0530

    Update video_list_item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoListItem = ({video}) => {
 	const imageUrl = video.snippet.thumbnails.default.url;
 	return(
-	 <li className="list-group-item">
+	<li onClick={() => onVideoSelect(video)} className="list-group-item">
 	   <div className="video-list media">
 	   <div className="media-left">
 	   <img className="media-object" src={imageUrl}/>
