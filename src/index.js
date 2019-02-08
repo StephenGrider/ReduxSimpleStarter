a2d312a7945af455f6fd623786a586d60763dd0a
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-09 01:58:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ class App extends Component {
 	render() {
 	 return (
 	    <div>
-	      <SearchBar onSearchTermChange={term => videoSearch(term)}/>
+	      <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
 	      <VideoDetail video={this.state.selectedVideo}  />
 	      <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
