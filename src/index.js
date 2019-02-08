1b51d38272f4237b9a61c5952b4fc11ce0a078ec
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-08 14:12:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,10 +14,16 @@ class App extends Component {
     constructor(props){
      super(props);
 
-     this.state = { videos:[]};
+     this.state = { 
+     	videos:[],
+     	selectedVideo: null
+      };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-          this.setState({ videos});
+        YTSearch({key: API_KEY, term: 'steve jobs'}, (videos) => {
+          this.setState({ 
+          	videos: videos,
+          	selectedVideo: videos[0]
+          });
           //this.setState({ videos: videos});
         });
     }
@@ -28,7 +34,7 @@ class App extends Component {
 	 return (
 	    <div>
 	      <SearchBar />
-	      <VideoDetail video={this.state.video[0] />
+	      <VideoDetail video={this.state.selectedVideo}  />
 	      <VideoList videos={this.state.videos} />
 	    </div>
      );	
