3a3cc3c0edb2b6b6db8a5cf0155d4bff8d9930ad
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-09 02:10:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class App extends Component {
 
     videoSearch(term) {
 
-     YTSearch({key: API_KEY, term: 'term'}, (videos) => {
+     YTSearch({key: API_KEY, term: term}, (videos) => {
           this.setState({ 
           	videos: videos,
           	selectedVideo: videos[0]
